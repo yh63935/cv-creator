160960f80c989850081ec41d566d18575d383816
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-04-11 23:44:25 -0700

    Implement state lifting to App components to render EducationForm and EducationInfo with appropriate states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,57 @@
 import PersonalInfo from "./personal-info/PersonalInfo";
-import Education from "./Education";
-
+import EducationForm from "./EducationForm";
+import EducationInfo from "./EducationInfo";
+import { useState } from "react";
 function App() {
+  const [school, setSchool] = useState("");
+  const [degree, setDegree] = useState("");
+
+  const [startYear, setStartYear] = useState("");
+
+  const [endYear, setEndYear] = useState("");
+
+  const [GPA, setGPA] = useState("");
+
+  function handleChange(e) {
+    const { id, value } = e.target;
+
+    switch (id) {
+      case "school":
+        setSchool(value);
+        break;
+      case "degree":
+        setDegree(value);
+        break;
+      case "start-year":
+        setStartYear(value);
+        break;
+      case "end-year":
+        setEndYear(value);
+        break;
+      case "gpa":
+        setGPA(value);
+        break;
+    }
+  }
+
   return (
     <>
       <PersonalInfo />
-      <Education />
+      <EducationForm
+        school={school}
+        degree={degree}
+        startYear={startYear}
+        endYear={endYear}
+        GPA={GPA}
+        handleChange={handleChange}
+      />
+      <EducationInfo
+        school={school}
+        degree={degree}
+        startYear={startYear}
+        endYear={endYear}
+        GPA={GPA}
+      />
     </>
   );
 }
