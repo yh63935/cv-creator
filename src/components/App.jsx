be00be696310081fc102b10d9c2abf4ff765a73f
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-04-26 20:32:16 -0700

    Create handleAdd, handleUpdate, and edit handleSave event handlers to add or update entries depending on edit mode and edit id

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,37 @@ function App() {
   const [savedData, setSavedData] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editMode, setEditMode] = useState("add"); //'add' or 'update'
+  const [editId, setEditId] = useState(null);
+
+  function handleSave(formData) {
+    // If edit mode is add, add new formData as an entry to savedData
+    if (editMode === "add") {
+      setSavedData([...savedData, formData]);
+    } else {
+      // If edit mode is update, update data when saving
+      // If savedData has the same ID as the one being edited, update the entry with the new form data
+      // Otherwise, return the original entry
     setSavedData(
       savedData.map((record) => {
-        if (record.id === id) {
+          if (record.id === editId) {
           return formData;
         }
         return record;
       })
     );
+    }
+    setIsEditing(false);
+  }
+
+  function handleAdd() {
+    setIsEditing(true);
+    setEditMode("add");
+  }
+
+  function handleUpdate(id) {
+    setIsEditing(true);
+    setEditMode("update");
+    setEditId(id);
   }
 
   function handleDelete(id) {
