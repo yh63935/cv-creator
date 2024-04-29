5cef21cdd11753d6eec86ec79bcbbc755e64a728
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-04-28 17:45:20 -0700

    Create general form component with placeholder fields for now

---
+++ src/components/Form.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Field from "./Field";
+export default function Form({ onCancel, onSave }) {
+  const [sectionEntry, setSectionEntry] = useState({
+    school: "",
+    degree: "",
+    startYear: "",
+    endYear: "",
+    gpa: "",
+  });
+
+  function handleChange(e) {
+    const { id, value } = e.target;
+
+    setSectionEntry({
+      ...sectionEntry,
+      [id]: value,
+    });
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    // Add a unique id to the education entry
+    const sectionEntryWithId = { ...sectionEntry, id: uuidv4() };
+    onSave(sectionEntryWithId);
+
+    // Clear input values
+    setSectionEntry({
+      school: "",
+      degree: "",
+      startYear: "",
+      endYear: "",
+      gpa: "",
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <Field
+          type="text"
+          id="school"
+          onChange={handleChange}
+          value={sectionEntry.school}
+        />
+        <Field
+          type="text"
+          id="degree"
+          onChange={handleChange}
+          value={sectionEntry.degree}
+        />
+        <Field
+          type="date"
+          id="startYear"
+          onChange={handleChange}
+          value={sectionEntry.startYear}
+        />
+        <Field
+          type="date"
+          id="endYear"
+          onChange={handleChange}
+          value={sectionEntry.endYear}
+        />
+        <Field
+          type="number"
+          id="gpa"
+          onChange={handleChange}
+          value={sectionEntry.gpa}
+        />
+        <button type="submit">Save</button>
+        <button onClick={onCancel}>Cancel</button>
+      </form>
+    </div>
+  );
+}
