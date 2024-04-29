bfdd517b90f073b87b0670a11271a0f7864b6f66
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-04-28 18:17:39 -0700

    Fix bug by changing Field component prop name to match parent Form component onChange prop name:

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,14 +1,9 @@
 // Field component that renders a div with label and input based on label, type, value, handleChange props
-export default function Field({ label, type, value, handleChange }) {
+export default function Field({ label, type, value, onChange }) {
   return (
     <div>
       <label htmlFor={label}></label>
-      <input
-        id={label}
-        type={type}
-        value={value}
-        onChange={handleChange}
-      ></input>
+      <input id={label} type={type} value={value} onChange={onChange}></input>
     </div>
   );
 }
