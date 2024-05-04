52a966f930b8850d4516be805d3891b6ba9f66f6
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-05-04 12:06:21 -0700

    Created SectionInfo component to dynamically render section data entries Each entry is displayed as a list item with its fields rendered as paragraph elements, excluding the 'id' field Each list item has its own edit and delete buttons

---
+++ src/components/SectionInfo.jsx
@@ -0,0 +1,28 @@
+// Section Info component that dynamically renders section data into an unordered list
+// Each list item has a Edit and Delete button to modify the entry in the section data
+export default function SectionInfo({ onUpdate, onDelete, sectionData }) {
+  return (
+    <ul>
+      {sectionData.map((sectionDataEntry) => {
+        return (
+          <li key={sectionDataEntry.id}>
+            {/* Render the fields of the section if the field is not id */}
+            {Object.entries(sectionDataEntry).map(([field, value]) => {
+              return (
+                field !== "id" && (
+                  <p key={field} className={field}>
+                    {value}
+                  </p>
+                )
+              );
+            })}
+            <button onClick={() => onUpdate(sectionDataEntry.id)}>Edit</button>
+            <button onClick={() => onDelete(sectionDataEntry.id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
